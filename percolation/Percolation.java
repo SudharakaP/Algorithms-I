7965a3d105ce0b1379076a81efe548601a94f48c
Sudharaka <sudharakap@mun.ca>
2019-03-01 22:01:42 -0800

    Create the percolation data type

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,61 +1,72 @@
 /* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
+ *  Name: Percolation
+ *  Date: 2019-03-01
+ *  Description: Creates the Percolation data type using Weighted Quick Union
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    WeightedQuickUnionUF weightedQuickUnionUF = new WeightedQuickUnionUF(n);
-    int[][] grid;
+    WeightedQuickUnionUF weightedQuickUnionUF;
+    int size;
     int numberOfOpenSites;
+    boolean[][] openClose;
 
     public Percolation(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException();
         }
-        grid = new int[n][n];
-        int count = 1;
+        size = n;
+        openClose = new boolean[n][n];
+        weightedQuickUnionUF = new WeightedQuickUnionUF(n * n + 2);
+        for (int i = 1; i <= n; i++)
+            weightedQuickUnionUF.union(0, i);
+        for (int i = n * n - n; i <= n * n; i++)
+            weightedQuickUnionUF.union(i, n * n + 1);
         for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; i++)
-                grid[i][j] = count++;
+            for (int j = 0; j < n; j++)
+                openClose[i][j] = false;
     }
 
     public void open(int row, int col) {
-        if ((row <= 1 || row >= grid.length) || (col <= 1 || col >= grid.length)) {
+        if ((row < 1 || row > size) || (col < 1 || col > size)) {
             throw new IllegalArgumentException();
         }
         if (!isOpen(row, col)) {
-            grid[row - 1][col - 1] = true;
+            if (col != 1)
+                weightedQuickUnionUF.union(size * (row - 1) + col, size * (row - 1) + col - 1);
+            if (col != size)
+                weightedQuickUnionUF.union(size * (row - 1) + col, size * (row - 1) + col + 1);
+            if (row != 1)
+                weightedQuickUnionUF.union(size * (row - 2) + col, size * (row - 1) + col);
+            if (row != size)
+                weightedQuickUnionUF.union(size * row + col, size * (row - 1) + col);
+            openClose[row - 1][col - 1] = true;
             numberOfOpenSites++;
         }
     }
 
     public boolean isOpen(int row, int col) {
-        if ((row <= 1 || row >= grid.length) || (col <= 1 || col >= grid.length)) {
+        if ((row < 1 || row > size) || (col < 1 || col > size)) {
             throw new IllegalArgumentException();
         }
 
-        return grid[row - 1][col - 1];
+        return openClose[row - 1][col - 1];
     }
 
-    // is site (row, col) full?
     public boolean isFull(int row, int col) {
-        if ((row <= 1 || row >= grid.length) || (col <= 1 || col >= grid.length)) {
+        if ((row < 1 || row > size) || (col < 1 || col > size)) {
             throw new IllegalArgumentException();
         }
-
+        return weightedQuickUnionUF.connected(0, size * (row - 1) + col);
     }
 
-    // number of open sites
     public int numberOfOpenSites() {
         return numberOfOpenSites;
     }
 
-    // does the system percolate?
     public boolean percolates() {
-        return false;
+        return weightedQuickUnionUF.connected(0, size * size + 1);
     }
 
     public static void main(String[] args) {
