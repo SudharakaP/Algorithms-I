347d71dae408185962fab321846c4079f9157a6a
Sudharaka <sudharakap@mun.ca>
2019-03-02 19:21:10 -0800

    Reduce calls to mean() and stddev() functions

--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -11,6 +11,8 @@ public class PercolationStats {
     private static final double CONFIDENCE_95 = 1.96;
     private final double[] fractionOfOpenSites;
     private final int numTrials;
+    private double mean;
+    private double stddev;
 
     public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0) {
@@ -28,11 +30,15 @@ public class PercolationStats {
     }
 
     public double mean() {
-        return StdStats.mean(fractionOfOpenSites);
+        if (mean == 0)
+            mean = StdStats.mean(fractionOfOpenSites);
+        return mean;
     }
 
     public double stddev() {
-        return StdStats.stddev(fractionOfOpenSites);
+        if (stddev == 0)
+            stddev = StdStats.stddev(fractionOfOpenSites);
+        return stddev;
     }
 
     public double confidenceLo() {
