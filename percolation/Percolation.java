7aa4292ecac497cae51edadb1fcd99a21b6f5f29
Sudharaka <sudharakap@mun.ca>
2019-03-02 19:09:19 -0800

    Percolation initialization mistake correction

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -7,8 +7,8 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private final WeightedQuickUnionUF WEIGHTED_QUICK_UNION_UF;
-    private final int SIZE;
+    private final WeightedQuickUnionUF weightedQuickUnionUF;
+    private final int size;
     private int numberOfOpenSites;
     private boolean[][] openClose;
 
@@ -16,13 +16,13 @@ public class Percolation {
         if (n <= 0) {
             throw new IllegalArgumentException();
         }
-        SIZE = n;
+        size = n;
         openClose = new boolean[n][n];
-        WEIGHTED_QUICK_UNION_UF = new WeightedQuickUnionUF(n * n + 2);
+        weightedQuickUnionUF = new WeightedQuickUnionUF(n * n + 2);
         for (int i = 1; i <= n; i++)
-            WEIGHTED_QUICK_UNION_UF.union(0, i);
-        for (int i = n * n - n; i <= n * n; i++)
-            WEIGHTED_QUICK_UNION_UF.union(i, n * n + 1);
+            weightedQuickUnionUF.union(0, i);
+        for (int i = n * n - n + 1; i <= n * n; i++)
+            weightedQuickUnionUF.union(i, n * n + 1);
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 openClose[i][j] = false;
@@ -32,17 +32,17 @@ public class Percolation {
         boundCheck(row, col);
         if (!isOpen(row, col)) {
             if (col != 1 && isOpen(row, col - 1))
-                WEIGHTED_QUICK_UNION_UF.union(twoToOneDimensionArray(row, col),
-                                              twoToOneDimensionArray(row, col - 1));
-            if (col != SIZE && isOpen(row, col + 1))
-                WEIGHTED_QUICK_UNION_UF.union(twoToOneDimensionArray(row, col),
-                                              twoToOneDimensionArray(row, col + 1));
+                weightedQuickUnionUF.union(twoToOneDimensionArray(row, col),
+                                           twoToOneDimensionArray(row, col - 1));
+            if (col != size && isOpen(row, col + 1))
+                weightedQuickUnionUF.union(twoToOneDimensionArray(row, col),
+                                           twoToOneDimensionArray(row, col + 1));
             if (row != 1 && isOpen(row - 1, col))
-                WEIGHTED_QUICK_UNION_UF.union(twoToOneDimensionArray(row - 1, col),
-                                              twoToOneDimensionArray(row, col));
-            if (row != SIZE && isOpen(row + 1, col))
-                WEIGHTED_QUICK_UNION_UF.union(twoToOneDimensionArray(row + 1, col),
-                                              twoToOneDimensionArray(row, col));
+                weightedQuickUnionUF.union(twoToOneDimensionArray(row - 1, col),
+                                           twoToOneDimensionArray(row, col));
+            if (row != size && isOpen(row + 1, col))
+                weightedQuickUnionUF.union(twoToOneDimensionArray(row + 1, col),
+                                           twoToOneDimensionArray(row, col));
             openClose[row - 1][col - 1] = true;
             numberOfOpenSites++;
         }
@@ -57,7 +57,7 @@ public class Percolation {
         boundCheck(row, col);
         if (!isOpen(row, col))
             return false;
-        return WEIGHTED_QUICK_UNION_UF.connected(0, SIZE * (row - 1) + col);
+        return weightedQuickUnionUF.connected(0, size * (row - 1) + col);
     }
 
     public int numberOfOpenSites() {
@@ -65,16 +65,16 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        return WEIGHTED_QUICK_UNION_UF.connected(0, SIZE * SIZE + 1);
+        return weightedQuickUnionUF.connected(0, size * size + 1);
     }
 
     private void boundCheck(int row, int col) {
-        if ((row < 1 || row > SIZE) || (col < 1 || col > SIZE)) {
+        if ((row < 1 || row > size) || (col < 1 || col > size)) {
             throw new IllegalArgumentException();
         }
     }
 
     private int twoToOneDimensionArray(int row, int col) {
-        return SIZE * (row - 1) + col;
+        return size * (row - 1) + col;
     }
 }
