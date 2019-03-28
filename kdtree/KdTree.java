2779879ff55446d3f0befd83be70a8f4b6a45cc2
Sudharaka <sudharakap@mun.ca>
2019-03-27 20:28:30 -0700

    Correct construction of the 2-d tree and visualization

--- kdtree/KdTree.java
+++ kdtree/KdTree.java
@@ -6,37 +6,67 @@
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
 
 import java.util.TreeSet;
 
 public class KdTree {
-    private TreeSet<Point2D> points;
+    private TreeSet<Node> nodes;
     private Node root;
 
+    public KdTree() {
+        nodes = new TreeSet<>();
+    }
+
     public boolean isEmpty() {
-        return points.size() == 0;
+        return nodes.isEmpty();
     }
 
     public int size() {
-        return points.size();
+        return nodes.size();
     }
 
     public void insert(Point2D p) {
         if (p == null)
             throw new IllegalArgumentException();
-        if (isEmpty())
+        if (isEmpty()) {
             root = new Node(p, null, null, null, 0);
+            root.rectHV = new RectHV(0, 0, 1, 1);
+            nodes.add(root);
+        }
         else {
             insertNode(new Node(p, null, null, null, 0), root);
         }
-        points.add(p);
     }
 
     private void insertNode(Node node, Node parent) {
-        if ((node.compareTo(parent) > 0 && parent.rightChild == null) || (node.compareTo(parent) < 0
-                && parent.leftChild == null)) {
+        if (node.compareTo(parent) > 0 && parent.rightChild == null) {
             node.parent = parent;
             node.depth = parent.depth + 1;
+            node.parent.rightChild = node;
+
+            if (parent.depth % 2 == 0)
+                node.rectHV = new RectHV(parent.point.x(), parent.rectHV.ymin(),
+                                         parent.rectHV.xmax(), parent.rectHV.ymax());
+            else
+                node.rectHV = new RectHV(parent.rectHV.xmin(), parent.point.y(),
+                                         parent.rectHV.xmax(), parent.rectHV.ymax());
+            nodes.add(node);
+            return;
+        }
+
+        if (node.compareTo(parent) < 0 && parent.leftChild == null) {
+            node.parent = parent;
+            node.depth = parent.depth + 1;
+            node.parent.leftChild = node;
+
+            if (parent.depth % 2 == 0)
+                node.rectHV = new RectHV(parent.rectHV.xmin(), parent.rectHV.ymin(),
+                                         parent.point.x(), parent.rectHV.ymax());
+            else
+                node.rectHV = new RectHV(parent.rectHV.xmin(), parent.rectHV.ymin(),
+                                         parent.rectHV.xmax(), parent.point.y());
+            nodes.add(node);
             return;
         }
 
@@ -54,11 +84,31 @@ public class KdTree {
     public boolean contains(Point2D p) {
         if (p == null)
             throw new IllegalArgumentException();
-        return points.contains(p);
+        return nodes.contains(new Node(p, null, null, null, 0)) || nodes
+                .contains(new Node(p, null, null, null, 1));
     }
 
     public void draw() {
+        Node currentNode = root;
+        for (Node node : nodes) {
+            drawNode(node);
+        }
+    }
 
+
+    private void drawNode(Node node) {
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+        node.point.draw();
+        StdDraw.setPenRadius();
+        if (node.depth % 2 == 0) {
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.line(node.point.x(), node.rectHV.ymin(), node.point.x(), node.rectHV.ymax());
+        }
+        else {
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.line(node.rectHV.xmin(), node.point.y(), node.rectHV.xmax(), node.point.y());
+        }
     }
 
     public Iterable<Point2D> range(RectHV rect) {
@@ -100,6 +150,7 @@ public class KdTree {
         private Node parent;
         private Node leftChild;
         private Node rightChild;
+        private RectHV rectHV;
         private int depth;
 
         public Node(Point2D point, Node parent, Node leftChild, Node rightChild, int depth) {
@@ -112,7 +163,7 @@ public class KdTree {
 
         @Override
         public int compareTo(Node node) {
-            if (depth % 2 == 0)
+            if (node.depth % 2 == 0)
                 return Double.compare(this.point.x(), node.point.x());
             else
                 return Double.compare(this.point.y(), node.point.y());
